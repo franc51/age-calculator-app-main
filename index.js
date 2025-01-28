33ee4ae85aefd9ac9014413ee5d5632789cb8815
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-01-28 10:55:11 +0200

    started error validation

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const day_input = document.getElementById('day_input');
+const day_label = document.getElementById('day_label');
 
 const submit_button = document.getElementById('submit_button');
+
+const error_message = document.getElementById('age__error_state')
 
 submit_button.addEventListener('click', () => {
     const day = parseInt(day_input.value, 10);
     if(day > 0 && day <= 31) {
-        console.log('Day is', day);
+        age__error_state.style.display = 'none';
+        day_input.style.boxShadow = '0 0 4px green';
+        day_label.style.color = 'green';
+       
     }
     else {
-        console.log('Please enter a valid day(1-31).');
+        age__error_state.style.display = 'block';
+        day_input.style.boxShadow = '0 0 4px red';
+        day_label.style.color = 'red';
     }
 })
